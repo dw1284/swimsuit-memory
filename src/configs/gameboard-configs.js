89fd532c35493aad05a1eef514a054b0021010a0
Dennis Williams <d_williams15@hotmail.com>
2020-08-31 23:06:24 -0500

    Added new gameboards

--- src/configs/gameboard-configs.js
+++ src/configs/gameboard-configs.js
@@ -2,24 +2,56 @@ module.exports = [
   {
     name: 'Lola',
     backgroundClassName: 'lola',
-    backgroundHeight: '720px',
-    backgroundWidth: '324px',
+    backgroundHeight: '714px',
+    backgroundWidth: '312px',
     tilesPerRow: 4,
     tilesPerColumn: 6
   },
+  // {
+  //   name: 'Marge',
+  //   backgroundClassName: 'marge',
+  //   backgroundHeight: '340px',
+  //   backgroundWidth: '798px',
+  //   tilesPerRow: 6,
+  //   tilesPerColumn: 4
+  // },
   {
     name: 'Marge',
-    backgroundClassName: 'marge',
-    backgroundHeight: '340px',
-    backgroundWidth: '798px',
-    tilesPerRow: 6,
-    tilesPerColumn: 4
+    backgroundClassName: 'marge2',
+    backgroundHeight: '696px',
+    backgroundWidth: '348px',
+    tilesPerRow: 4,
+    tilesPerColumn: 6
   },
   {
     name: 'Olive Oil',
     backgroundClassName: 'olive-oil',
     backgroundHeight: '696px',
     backgroundWidth: '276px',
+    tilesPerRow: 4,
+    tilesPerColumn: 6
+  },
+  {
+    name: 'Wilma',
+    backgroundClassName: 'wilma',
+    backgroundHeight: '720px',
+    backgroundWidth: '261px',
+    tilesPerRow: 3,
+    tilesPerColumn: 6
+  },
+  {
+    name: 'Homer',
+    backgroundClassName: 'homer',
+    backgroundHeight: '732px',
+    backgroundWidth: '364px',
+    tilesPerRow: 4,
+    tilesPerColumn: 6
+  },
+  {
+    name: 'Barney',
+    backgroundClassName: 'barney',
+    backgroundHeight: '516px',
+    backgroundWidth: '308px',
     tilesPerRow: 4,
     tilesPerColumn: 6
   }
