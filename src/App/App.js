876b50c290bc2d13bb735ea759f0d0588f398579
Dennis Williams <d_williams15@hotmail.com>
2020-09-02 05:04:43 -0500

    Optimization: Preload card images so that the experience is more seamless for user and more efficient

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import _ from 'lodash';
 import './App.css';
 import StartScreen from './StartScreen/StartScreen';
 import Gameboard from './Gameboard/Gameboard';
+import cardConfigs from '../configs/card-configs';
 
 class App extends React.Component {
   constructor(props) {
@@ -20,15 +22,29 @@ class App extends React.Component {
     this.setState({gameboardConfig: null});
   };
   
+  generateElementsToPreload = () => {
+    // Optimization hack:
+    // Preload all card images into img elements
+    // This guarantees two things:
+    //  1. All images will be loaded up front and the user won't experience lag when clicking cards
+    //  2. We only download the images once per session...no matter how many times the user plays the game
+    return (
+      <div className="preload">
+        {_.map(cardConfigs, cardUrl => <img key={_.uniqueId()} src={cardUrl} />)}
+      </div>
+    );
+  };
+  
   render() {
     const {gameboardConfig} = this.state;
-    
+    const preloadElement = this.generateElementsToPreload();
     const renderElement = gameboardConfig
       ? <Gameboard gameboardConfig={gameboardConfig} onNewGameClick={this.onNewGameClick} />
       : <StartScreen onStartGameClick={this.onStartGameClick} />;
-      
+    
     return (
       <div className="app">
+        {preloadElement}
         {renderElement}
       </div>
     );
