09a94f136a07572615fe520a477adb53b9add1b0
Dennis Williams <d_williams15@hotmail.com>
2021-02-22 02:22:35 -0600

    Added scrolling text animation to gameboard selection option

--- src/components/OptionScroller/OptionScroller.js
+++ src/components/OptionScroller/OptionScroller.js
@@ -6,38 +6,67 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     
+    const options = this.generateOptionObjects(props.options, props.selectedOption);
+    const selectedOption = _.find(options, {text: props.selectedOption});
+    
     this.state = {
-      options: props.options,
-      selectedOption: props.selectedOption,
+      options,
+      selectedOption
     };
   }
   
+  generateOptionObjects = (options, selectedOption) => {
+    return _.map(options, option => {
+      return {
+        text: option,
+        animationClassName: option !== selectedOption ? 'enter-from-left' : null
+      }
+    });
+  };
+  
   onPrevClick = () => {
-    const {options, selectedOption} = this.state;
-    const currIndex = options.indexOf(selectedOption);
+    const {options, selectedOption: previouslySelectedOption} = this.state;
+    const currIndex = _.findIndex(options, {text: previouslySelectedOption.text});
     const newSelectedOption = options[(currIndex + options.length - 1) % options.length];
-    this.setState({selectedOption: newSelectedOption}, () => {this.props.onChange(newSelectedOption)});
+    
+    previouslySelectedOption.animationClassName = 'exit-left';
+    newSelectedOption.animationClassName = 'enter-from-right';
+    
+    this.setState({selectedOption: previouslySelectedOption}, () => {
+      setTimeout(() => {
+        this.setState({selectedOption: newSelectedOption}, () => {
+          this.props.onChange(newSelectedOption.text)
+        });
+      }, 125)
+    });
   };
   
   onNextClick = () => {
-    const {options, selectedOption} = this.state;
-    const currIndex = options.indexOf(selectedOption);
-    const newSelectedOption = options[(currIndex + 1) % options.length];
-    this.setState({selectedOption: newSelectedOption}, () => {this.props.onChange(newSelectedOption)});
+    const {options, selectedOption: previouslySelectedOption} = this.state;
+    const currIndex = _.findIndex(options, {text: previouslySelectedOption.text});
+    const newSelectedOption = options[(currIndex + options.length + 1) % options.length];
+    
+    previouslySelectedOption.animationClassName = 'exit-right';
+    newSelectedOption.animationClassName = 'enter-from-left';
+    
+    this.setState({selectedOption: previouslySelectedOption}, () => {
+      setTimeout(() => {
+        this.setState({selectedOption: newSelectedOption}, () => {
+          this.props.onChange(newSelectedOption.text)
+        });
+      }, 125)
+    });
   };
   
   render() {
-    const {options, selectedOption} = this.state;
-    
-    if (!_.includes(options, selectedOption)) {
-      throw new Error(`The value provided for selectedOption prop (${selectedOption}) is not included in the options array (${options}).`);
-    }
+    const {selectedOption} = this.state;
+    const {text: selectedOptionDisplayText, animationClassName} = selectedOption;
     
     return (
       <div className="option-scroller">
         <button type="button" onClick={this.onPrevClick}>&lt;</button>
-        <div className="selected-option-label">
-          {this.state.selectedOption}
+        <div className={`selected-option-label ${animationClassName ? animationClassName : ''}`}>
+          {selectedOptionDisplayText}
         </div>
         <button type="button" onClick={this.onNextClick}>&gt;</button>
       </div>
